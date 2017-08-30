2b301386f6f4dd4e04955a55e1e8871b73dd2ec9
StevenZack <zigzigcheers@gmail.com>
2017-08-30 13:56:44 +0800

    m

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 let version='0.3';
+let cacheStorageKey="note"
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('airhorner').then(cache => {
+    caches.open(cacheStorageKey).then(cache => {
       return cache.addAll([
+        `/pwa-note/`,
         `/pwa-note/index.html`
       ])
       .then(() => self.skipWaiting());
@@ -13,11 +15,32 @@ self.addEventListener('install', e => {
 self.addEventListener('activate',  event => {
   event.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
+// self.addEventListener('activate', function(e) {
+//   e.waitUntil(
+//     Promise.all(
+//       caches.keys().then(cacheNames => {
+//         return cacheNames.map(name => {
+//           if (name !== cacheStorageKey) {
+//             return caches.delete(name)
+//           }
+//         })
+//       })
+//     ).then(() => {
+//       return self.clients.claim()
+//     })
+//   )
+// })
+// self.addEventListener('fetch', event => {
+//   event.respondWith(
+//     caches.match(event.request, {ignoreSearch:true}).then(response => {
+//       return response;
+//     })
+//   );
+// });
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+        return response
     })
-  );
-});
+  )
+})
